b6ce49ebd385cb1f3bf89fc4fd24883247dba0bb
nazim32 <chowdhurynazim32@gmail.com>
2025-10-03 23:33:13 +0600

    DOM is completed.

--- intermediate/DOM APIs/dom2.js
+++ intermediate/DOM APIs/dom2.js
@@ -2,3 +2,9 @@ let divElement = document.querySelector('#container2');
 console.log(divElement);
 
 console.log("This is the dom2.bjs file");
+
+//parentNode.replaceChild(new , old);
+//target.replaceWith(current);
+//parentNode.removeChild(targethildForRemove);
+//directRemove.remove();
+
